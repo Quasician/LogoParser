09bf865f6060b6312d48fd15e81aa7ebc0ea5e6c
Himanshu Jain <hkj4@dj4.edu>
2020-02-21 19:57:34 -0500

    MAde changes in the Visualizer

--- src/slogo/Main.java
+++ src/slogo/Main.java
@@ -1,13 +1,26 @@
 package slogo;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import slogo.View.Visualiser;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
-public class Main {
+public class Main extends Application {
+    public static final String TITLE = "Slogo";
     /**
      * Start of the program.
      */
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setResizable(false);
+        primaryStage.setTitle(TITLE);
+        Visualiser logo = new Visualiser();
+        primaryStage.setScene(logo.Screen());
+        primaryStage.show();
+    }
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        launch(args);
     }
 }
---
+++ src/slogo/View/Visualiser.java
@@ -0,0 +1,20 @@
+package slogo.View;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+public class Visualiser {
+    private static final int SCREEN_WIDTH = 1200;
+    private static final int SCREEN_HEIGHT = 800;
+    private static final Paint SCREEN_BACKGROUND = Color.web("0e1e38");
+
+
+    public Scene Screen(){
+        VBox root = new VBox(20);
+        root.setAlignment(Pos.CENTER);
+        return new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BACKGROUND);
+    }
+}
