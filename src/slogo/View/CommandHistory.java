0149ed0aaf5fdfcfa2003883385e79a708fb3252
Himanshu Jain <hkj4@dj4.edu>
2020-02-22 18:42:35 -0500

    Made the CommandHistory

--- src/slogo/View/CommandHistory.java
+++ src/slogo/View/CommandHistory.java
@@ -1,4 +1,39 @@
 package slogo.View;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class CommandHistory {
+    private static VBox historyWindow;
+    private HBox newCommand;
+    private CustomButton runButton;
+    private Label commandEntered;
+    private static final String imgValue= "Play.png";
+    private static final int spacing=10;
+    private static final int BoxSpacing=5;
+
+    public CommandHistory(){
+        runButton=new CustomButton();
+        historyWindow=new VBox(spacing);
+        runButton.setImage(runButton,imgValue);
+
+    }
+    private void runCommand(HBox cellForWindow){
+        //run the command in the box of the runButton using the backend
+        //doCommand(cellForWindow.getAccessibleText());
+    }
+    
+    private void makeBox(String command){
+        newCommand= new HBox(BoxSpacing);
+        commandEntered=new Label(command);
+        newCommand.getChildren().addAll(commandEntered,runButton);
+        newCommand.setAccessibleText(command);
+        historyWindow.getChildren().add(newCommand);
+        runButton.setOnAction(actionEvent -> runCommand(newCommand));
+    }
+
+    public static VBox returnScene(){
+        return historyWindow;
+    }
 }
--- src/slogo/View/Visualiser.java
+++ src/slogo/View/Visualiser.java
@@ -1,6 +1,5 @@
 package slogo.View;
 
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -10,14 +9,15 @@ public class Visualiser {
     private static final int SCREEN_WIDTH = 1200;
     private static final int SCREEN_HEIGHT = 800;
     private static Toolbar bar;
+    private static CommandHistory window;
     private static final Paint SCREEN_BACKGROUND = Color.web("0e1e38");
 
 
     public Scene Screen(){
         VBox root = new VBox(20);
         bar=new Toolbar();
-
-        root.getChildren().add(bar.ToolBar());
+        window= new CommandHistory();
+        root.getChildren().addAll(bar.ToolBar(), window.returnScene());
         return new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BACKGROUND);
     }
 }
