0bc655a2e013fd5fe63cf1831e2ea4be9a018860
Himanshu Jain <hkj4@dj4.edu>
2020-02-21 21:05:03 -0500

    MAde changes in the Visualizer

--- src/slogo/View/Toolbar.java
+++ src/slogo/View/Toolbar.java
@@ -3,7 +3,9 @@ package slogo.View;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
@@ -13,32 +15,43 @@ public class Toolbar {
     private Button help;
     private Button setTurtleImage;
     private Button changeLanguage;
+    private Button setBackground;
+    private Button setPen;
     private HBox toolBar;
+    private VBox dropDown;
+    private final ComboBox colorComboBox;
     private Drawing changeProperties;
     private static final Paint BUTTON_FONT_COLOR = Color.BLACK;
-    private static final int BUTTON_FONT_SIZE = 16;
+    private static final int BUTTON_FONT_SIZE = 13;
     private static final String BUTTON_BACKGROUND = "Change the Background Color";
     private static final String BUTTON_PEN = "Change the Pen Color";
     private static final String BUTTON_HELP ="Help";
     private static final String BUTTON_TURTLE = "Change the Turtle Image";
     private static final String BUTTON_LANG ="Change Language";
     private static final String STYLE_COLOR ="lightgray";
-    private static final int PADDING = 50;
+    private static final int PADDING = 20;
 
     public Toolbar(){
         backgroundColor= CustomButton.pickColor(BUTTON_BACKGROUND);
-        changeProperties=new Drawing();
         backgroundColor.setOnAction(e->changeProperties.changeBackground(backgroundColor.getValue()));
         penColor= CustomButton.pickColor(BUTTON_PEN);
         penColor.setOnAction(e->changeProperties.changePen(penColor.getValue()));
+        setBackground= CustomButton.CustomButton(BUTTON_BACKGROUND, STYLE_COLOR, BUTTON_FONT_COLOR, BUTTON_FONT_SIZE);
+        setPen= CustomButton.CustomButton(BUTTON_PEN, STYLE_COLOR, BUTTON_FONT_COLOR, BUTTON_FONT_SIZE);
+        changeProperties=new Drawing();
+         colorComboBox = new ComboBox();
+        colorComboBox.getItems().addAll(
+                setBackground,setPen
+        );
+
         help= CustomButton.CustomButton(BUTTON_HELP, STYLE_COLOR, BUTTON_FONT_COLOR, BUTTON_FONT_SIZE);
         setTurtleImage= CustomButton.CustomButton(BUTTON_TURTLE, STYLE_COLOR, BUTTON_FONT_COLOR, BUTTON_FONT_SIZE);
         changeLanguage= CustomButton.CustomButton(BUTTON_LANG, STYLE_COLOR, BUTTON_FONT_COLOR, BUTTON_FONT_SIZE);
     }
 
     public HBox ToolBar(){
-        toolBar=new HBox(20);
-        toolBar.getChildren().addAll(backgroundColor,penColor,help,setTurtleImage,changeLanguage);
+        toolBar=new HBox();
+        toolBar.getChildren().addAll(help,setTurtleImage,changeLanguage,setPen,setBackground,colorComboBox);
         toolBar.setPadding(new Insets(PADDING));
         return toolBar;
     }
--- src/slogo/View/Visualiser.java
+++ src/slogo/View/Visualiser.java
@@ -9,12 +9,15 @@ import javafx.scene.paint.Paint;
 public class Visualiser {
     private static final int SCREEN_WIDTH = 1200;
     private static final int SCREEN_HEIGHT = 800;
+    private static Toolbar bar;
     private static final Paint SCREEN_BACKGROUND = Color.web("0e1e38");
 
 
     public Scene Screen(){
         VBox root = new VBox(20);
-        root.setAlignment(Pos.CENTER);
+        bar=new Toolbar();
+
+        root.getChildren().add(bar.ToolBar());
         return new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BACKGROUND);
     }
 }
