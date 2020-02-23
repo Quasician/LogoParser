801379d014dbeb99d824f7c00d52d5e71e8d05ef
Himanshu Jain <hkj4@dj4.edu>
2020-02-22 19:49:16 -0500

    Made the VariableHistory class

--- src/slogo/View/CommandHistory.java
+++ src/slogo/View/CommandHistory.java
@@ -3,6 +3,9 @@ package slogo.View;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import slogo.model.Command;
+
+import java.util.LinkedHashMap;
 
 public class CommandHistory {
     private static VBox historyWindow;
@@ -12,23 +15,26 @@ public class CommandHistory {
     private static final String imgValue= "Play.png";
     private static final int spacing=10;
     private static final int BoxSpacing=5;
+    private LinkedHashMap<String,Command> Values;
 
     public CommandHistory(){
         runButton=new CustomButton();
+        Values= new LinkedHashMap<>();
         historyWindow=new VBox(spacing);
         runButton.setImage(runButton,imgValue);
-
     }
+
     private void runCommand(HBox cellForWindow){
         //run the command in the box of the runButton using the backend
-        //doCommand(cellForWindow.getAccessibleText());
+        //doCommand(Values.get(cellForWindow.getAccessibleText()));
     }
 
-    private void makeBox(String command){
+    private void makeBox(Command command, String StringRepresentation){
+        Values.put(StringRepresentation, command);
         newCommand= new HBox(BoxSpacing);
-        commandEntered=new Label(command);
+        commandEntered=new Label(StringRepresentation);
         newCommand.getChildren().addAll(commandEntered,runButton);
-        newCommand.setAccessibleText(command);
+        newCommand.setAccessibleText(StringRepresentation);
         historyWindow.getChildren().add(newCommand);
         runButton.setOnAction(actionEvent -> runCommand(newCommand));
     }
--- src/slogo/View/VariableHistory.java
+++ src/slogo/View/VariableHistory.java
@@ -1,4 +1,32 @@
 package slogo.View;
 
+import javax.lang.model.element.Element;
+import java.util.HashMap;
+
 public class VariableHistory {
+    private HashMap<String, Element> Variables;
+
+    public VariableHistory(){
+        Variables=new HashMap<>();
+    }
+
+    public void addVariable(String name, Element value){
+        for (String count:Variables.keySet()){
+            if(count.equals(name)){
+                Variables.replace(name,value);
+            }else {
+                Variables.putIfAbsent(name, value);
+            }
+        }
+    }
+
+    public Element getVariable(String name){
+        for(String count:Variables.keySet()){
+            if(count.equals(name)){
+                return Variables.get(count);
+            }
+        }
+        return null;
+    }
+
 }
