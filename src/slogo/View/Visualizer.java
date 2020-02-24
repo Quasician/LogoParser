82fc02ffb0f30562b95e6ad98f450c49405eea64
Michelle Tai <michelle.tai@duke.edu>
2020-02-24 16:55:59 -0500

    Cleaned up the code in visualizer

--- src/slogo/View/Visualizer.java
+++ src/slogo/View/Visualizer.java
@@ -12,21 +12,18 @@ import javax.lang.model.element.Element;
 import slogo.model.Turtle;
 
 public class Visualizer {
-
-  public static final String RESOURCES = "resources";
-  public static final String DEFAULT_RESOURCE_PACKAGE = RESOURCES + ".";
-//  public static final String LANGUAGE = "English";
-  public static final String DEFAULT_RESOURCE_FOLDER = "/" + RESOURCES + "/";
-//  public static ResourceBundle SIMULATION_RESOURCE = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + LANGUAGE);
-  public static ResourceBundle SIMULATION_RESOURCE = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "DisplayEnglish");
-
   private static int WINDOW_WIDTH = 1400;
   private static int WINDOW_HEIGHT = 1000;
   private Stage myWindow;
+
   private CommandHistory myCommandHistory;
   private VariableHistory myVariableHistory;
   private BorderPane bp;
   private Turtle viewTurtle;
+  private CommandLine cmdline;
+  private Drawing drawer;
+  private TurtleGrid grid;
+  private Toolbar tool;
 
   /**
    * Constructor for the visualizer class
@@ -38,26 +35,25 @@ public class Visualizer {
     myVariableHistory = new VariableHistory();
 
 //        ViewButton btn = new ViewButton("hi", 50, 100);
-    CommandLine cmdline = new CommandLine();
+    cmdline = new CommandLine();
     this.viewTurtle = viewTurtle;
-    Drawing drawer = new Drawing();
-    TurtleGrid grid = new TurtleGrid(viewTurtle, drawer);
-    Toolbar tool = new Toolbar(drawer, grid);
-
+    drawer = new Drawing();
+    grid = new TurtleGrid(viewTurtle, drawer);
+    tool = new Toolbar(drawer, grid);
     bp = new BorderPane();
-    bp.setBottom(cmdline.getCommandLineGroup());
-    bp.setLeft(grid.getTurtleGrid());
 
-    bp.setTop(tool.ToolBar());
-    VBox vbox = new VBox();
-    vbox.setAlignment(Pos.CENTER);
-
-    makeHistory();
+    setupBorderPane();
 
     Scene scene = new Scene(bp, WINDOW_WIDTH, WINDOW_HEIGHT);
-//    vbox.getChildren().addAll(tool.ToolBar(), cmdline.getCommandLineGroup());
     window.setScene(scene);
     window.show();
+  }
+
+  private void setupBorderPane() {
+    bp.setBottom(cmdline.getCommandLineGroup());
+    bp.setLeft(grid.getTurtleGrid());
+    bp.setTop(tool.ToolBar());
+    makeHistory();
   }
 
   private void makeHistory(){
