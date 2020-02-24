8b2e6b8e084e4b82d6793ee05f347d3b592c8973
Michelle Tai <michelle.tai@duke.edu>
2020-02-24 16:55:33 -0500

    Added labels to the color pickers

--- src/slogo/View/Toolbar.java
+++ src/slogo/View/Toolbar.java
@@ -1,13 +1,18 @@
 package slogo.View;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
 public class Toolbar {
-
   private ColorPicker backgroundColor;
   private ColorPicker penColor;
   private Button help;
