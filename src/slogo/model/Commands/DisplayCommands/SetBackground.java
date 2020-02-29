bc14b28db2762a8c39618449a66bcf4834ad8004
sas182 <sas182@duke.edu>
2020-02-29 12:54:35 -0500

    Refactoring back end to be more readable, smaller methods. Started with command tree executor. Deleted the command type hash map class, using property file instead

---
+++ src/slogo/model/Commands/DisplayCommands/SetBackground.java
@@ -0,0 +1,15 @@
+package slogo.model.Commands.DisplayCommands;
+
+import slogo.model.TreeNode;
+
+public class SetBackground extends DisplayCommand {
+
+  public SetBackground(String name) {
+    super(name);
+  }
+
+  @Override
+  public void doCommand(TreeNode commandNode) {
+
+  }
+}
