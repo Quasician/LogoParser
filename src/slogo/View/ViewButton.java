ce315e7fe340dd6a84f19a356f603f1a4c4d2c3f
Himanshu Jain <hkj4@dj4.edu>
2020-03-06 18:19:48 -0500

    Removed hardcoded values from Visualizer

--- src/slogo/View/ViewButton.java
+++ src/slogo/View/ViewButton.java
@@ -15,16 +15,17 @@ import javafx.scene.text.FontWeight;
 public class ViewButton extends Button {
   private static final String STYLE_COLOR = "lightgray";
   private static final int DEFAULT_FONT_SIZE = 20;
-  private static final int DEFAULT_BUTTON_WIDTH = 200;
-  private static final int DEFAULT_BUTTON_HEIGHT = 25;
-
+  private static final String fontFormat="Calibri";
+  private static final int DEFAULT_BUTTON_WIDTH = 65;
+  private static final int DEFAULT_BUTTON_HEIGHT =45;
+  private static final int DEFAULT_FONT_SIZE_SMALL =10;
   /**
    * Constructor method that lays out the basic view of a button in our parser
    * @param text specifies text in button that corresponds with the value left value in the properties file
    * @author Michelle Tai, adapted from Franklin Boampong's button code in our Simulation project
    */
   public ViewButton(String text, int height, int width, int fontSize) {
-    setFont(Font.font("Calibri", fontSize));
+    setFont(Font.font(fontFormat, fontSize));
     setText(text);
     setPrefHeight(height);//45
     setPrefWidth(width);//190
@@ -32,10 +33,10 @@ public class ViewButton extends Button {
     mouseUpdateListener();
   }
   public ViewButton(String text) {
-    setFont(Font.font("Calibri", 10));
+    setFont(Font.font(fontFormat, DEFAULT_FONT_SIZE_SMALL));
     setText(text);
-    setPrefHeight(45);//45
-    setPrefWidth(65);//190
+    setPrefHeight(DEFAULT_BUTTON_HEIGHT);//45
+    setPrefWidth(DEFAULT_BUTTON_WIDTH);//190
 //    setStyle("-fx-background-color:" + STYLE_COLOR + ";-fx-font-size:" + fontSize + " px;");
     mouseUpdateListener();
   }
