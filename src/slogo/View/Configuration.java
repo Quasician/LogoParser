6c8bf55d0d14bbd05d7d440ac8735653ca673bd4
Himanshu Jain <hkj4@dj4.edu>
2020-03-06 20:20:54 -0500

    removed hardcoded values from configuration

--- src/slogo/View/Configuration.java
+++ src/slogo/View/Configuration.java
@@ -1,53 +1,43 @@
 package slogo.View;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import slogo.model.DisplayOption;
 import slogo.model.Turtle;
-
+import java.util.ResourceBundle;
 public class Configuration {
-    private ListView variablesHolder;
     private ListView variablesTable;
-    private static final Paint TEXT_COLOR = Color.BLACK;
-    private Label varHistLabel;
-    private static final String LISTVIEW_STYLE = "-fx-background-color: rgba(255, 255, 255, 0.5);";
-    private static final double VAR_BOX_HEIGHT = 310.0;
-    private static final double VAR_BOX_WIDTH = 300.0;
-    private static final int VAR_LABEL_HEIGHT = 10;
-    private TableColumn nameCol;
-    private TableColumn valCol;
-    private VBox variableHist;
-    private SimpleStringProperty firstName;
-    private SimpleStringProperty lastName;
+    private static final String THIS_PACKAGE = Configuration.class.getPackageName();
+    private static final String MY_RESOURCE_FOLDER = THIS_PACKAGE + ".visualProperty.";
+    private static ResourceBundle turtleImages = ResourceBundle.getBundle(MY_RESOURCE_FOLDER + "PropertiesWindow");
+    private static final String TURTLE_NUMBER_TEXT= turtleImages.getString("TurtlesNumber");
+    private static final String PEN_THICKNESS= turtleImages.getString("PenThicknessText");
+    private static final String PEN_DOWN_PROP= turtleImages.getString("PenDownText");
+    private static final String BACKGROUND_COLOR_TEXT= turtleImages.getString("BackgroundColorText");
+    private static final String PEN_COLOR_TEXT= turtleImages.getString("PenColorText");
+    private static final String IMAGE_INDEX_TEXT= turtleImages.getString("ImageIndexText");
+    private static final String TURT_COORD_TEXT= turtleImages.getString("TurtleCoordText");
+    private static final String TURT_STATUS_TEXT= turtleImages.getString("TurtStatusText");
+    private static final String isString=" is ";
+    private static final String commaString=",";
     private Label PropertyName;
-    private Label number= new Label();
-    private Label color= new Label();
-    private Label size= new Label();
-    private Label penDown= new Label();
-    private Label penColor= new Label();
-    private Label turtleStatus= new Label();
-    private Label turtleCoord= new Label();
-    private Label imageIndex= new Label();
-    private HBox rowForSize;
-    private HBox CoordForSize;
-    public Configuration(String name, String value){
-        this.firstName = new SimpleStringProperty(name);
-        this.lastName = new SimpleStringProperty(value);
-    }
+    private Label number;
+    private Label color;
+    private Label size;
+    private Label penDown;
+    private Label penColor;
+    private Label turtleStatus;
+    private Label turtleCoord;
+    private Label imageIndex;
 
     public Configuration(ObservableList<Turtle> turtles, DisplayOption displayOption) {
         variablesTable = new ListView<>();
-        PropertyName=new Label("Number of Turtles ");
+        PropertyName=new Label();
         number= new Label();
         color= new Label();
         size= new Label();
@@ -62,10 +52,10 @@ public class Configuration {
     }
 
     public void initialDisplay(DisplayOption displayOption,ObservableList<Turtle> turtles){
-        color.setText(String.valueOf("Pen Thickness is: "+displayOption.getPenWidthProperty().getValue()));
-        size.setText(String.valueOf("Background Color Index is: "+displayOption.getBgIndex()));
-        penColor.setText("Pen Color Index is: "+ String.valueOf(displayOption.getPenIndex()));
-        imageIndex.setText("Image Index is: "+ String.valueOf(displayOption.getImageIndex()));
+        color.setText(PEN_THICKNESS +displayOption.getPenWidthProperty().getValue());
+        size.setText(BACKGROUND_COLOR_TEXT+displayOption.getBgIndex());
+        penColor.setText(PEN_COLOR_TEXT+ displayOption.getPenIndex());
+        imageIndex.setText(IMAGE_INDEX_TEXT+ displayOption.getImageIndex());
         int count=0;
         for(Turtle t: turtles){
             count++;
@@ -73,8 +63,8 @@ public class Configuration {
         if(variablesTable.getItems().contains(number)){
             variablesTable.getItems().remove(number);
         }
-        number.setText("Number of Turtles: "+ String.valueOf(count));
-        penDown.setText("Pen Down Property: "+ true);
+        number.setText(TURTLE_NUMBER_TEXT + count);
+        penDown.setText(PEN_DOWN_PROP+ true);
         variablesTable.getItems().addAll(number,color,size,penColor,imageIndex,penDown, turtleStatus, turtleCoord);
 
 
@@ -85,7 +75,7 @@ public class Configuration {
             for(Turtle t: turtles){
                     count++;
             }
-            number.setText("Number of Turtles: "+ String.valueOf(count));
+            number.setText(PEN_THICKNESS+ count);
             });
     }
 
@@ -95,47 +85,46 @@ public class Configuration {
 
     public void sendDisplay(DisplayOption displayOption) {
         displayOption.getPenWidthProperty().addListener(e->{
-            System.out.println("GOT TO HERE SEND DISPLAY");
-            color.setText(String.valueOf("Pen Thickness is: "+displayOption.getPenWidthProperty().getValue()));
+            color.setText(PEN_THICKNESS+displayOption.getPenWidthProperty().getValue());
         });
 
         displayOption.getBgIndex().addListener(e->{
-            size.setText(String.valueOf("Background Color Index is: "+displayOption.getBgIndex()));
+            size.setText(BACKGROUND_COLOR_TEXT+displayOption.getBgIndex());
 
         });
 
         displayOption.getPenIndex().addListener(e->{
-            penColor.setText("Pen Color Index is: "+ String.valueOf(displayOption.getPenIndex()));
+            penColor.setText(PEN_COLOR_TEXT+ displayOption.getPenIndex());
         });
 
         displayOption.getImageIndex().addListener(e->{
-            imageIndex.setText("Image Index is: "+ String.valueOf(displayOption.getImageIndex()));
+            imageIndex.setText(IMAGE_INDEX_TEXT+ displayOption.getImageIndex());
         });
     }
 
 
     public void sendPenColor(Paint penColorIndex) {
-        penColor.setText("Pen Color Index is: "+ String.valueOf(penColorIndex));
+        penColor.setText(PEN_COLOR_TEXT+ penColorIndex);
     }
 
     public void setTurtleIndex(Object value) {
-       imageIndex.setText("Image of Turtles is: "+ String.valueOf(value));
+       imageIndex.setText(IMAGE_INDEX_TEXT + value);
     }
 
     public void changeBackground(Color color) {
-        size.setText(String.valueOf("Background Color Index is: "+ color));
+        size.setText(BACKGROUND_COLOR_TEXT+ color);
     }
 
     public void changePenDown(Boolean isPenDown) {
-        penDown.setText("Pen Down Property: "+ String.valueOf(isPenDown));
+        penDown.setText(PEN_DOWN_PROP+ isPenDown);
     }
 
     public void changeActive(Turtle viewTurtle) {
-        turtleStatus.setText("The Turtle with id "+viewTurtle.getId()+ " is "+ viewTurtle.isActivatedProperty().getValue());
+        turtleStatus.setText(TURT_STATUS_TEXT+viewTurtle.getId()+ isString + viewTurtle.isActivatedProperty().getValue());
     }
 
     public void makeCoord(Turtle viewTurtle) {
-        turtleCoord.setText("The Turtle's Coordinates are "+ viewTurtle.getX()+ ","+ viewTurtle.getY());
+        turtleCoord.setText(TURT_COORD_TEXT+ viewTurtle.getX()+ commaString+ viewTurtle.getY());
     }
 }
 
