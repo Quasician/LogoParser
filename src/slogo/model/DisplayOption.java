acd5d8c0371fd8d1313d2ec1cc078723160f5865
sas182 <sas182@duke.edu>
2020-03-22 23:05:17 -0500

    finished commenting DisplayOption

--- src/slogo/model/DisplayOption.java
+++ src/slogo/model/DisplayOption.java
@@ -113,28 +113,53 @@ public class DisplayOption {
     imageIndex.set(index);
   }
 
+  /**
+   * Set the current background color based on the input index
+   * @param index
+   */
   public void setCurrentBackground(int index) {
     bgIndex.set(index);
   }
 
+  /**
+   * Set the current pen width based on the input width
+   * @param width
+   */
   public void setPenWidth(double width) {
     penWidth.set(width);
   }
 
+  /**
+   * Set the number of images available for turtles
+   * @param number
+   */
   public void setNumImages(int number) {
     numImages = number;
   }
 
+  /**
+   * Get the number of images available for turtles, used in error checking
+   */
   public int getNumImages() {
     return numImages;
   }
 
+  /**
+   * Add a new color to the list of color options. This is used in the SetPalette command, which
+   * allows users to add their own color to the options for background and pen color.
+   * @param c
+   */
   public void addToList(String c) {
     String[] colorarray = c.split(COMMA);
     String color = colorarray[0];
     colorOptions.add(color + COMMA + colorOptions.size());
   }
 
+  /**
+   * Used in SetPalette command to change the color at the input index
+   * @param index
+   * @param rgb int array of red, green, blue colors
+   */
   public void setColorAt(int index, int[] rgb) {
     String[] rgbStrings = new String[rgb.length];
     for (int i = 0; i < rgbStrings.length; i++) {
